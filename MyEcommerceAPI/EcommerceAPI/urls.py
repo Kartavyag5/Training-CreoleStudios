131f0895e7ab6e605199943443e44b369511d947
kartavyag5 <kartavyag5@gmail.com>
2021-07-10 13:25:10 +0530

    Some Changes in urls

--- MyEcommerceAPI/EcommerceAPI/urls.py
+++ MyEcommerceAPI/EcommerceAPI/urls.py
@@ -1,16 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
-#from rest_framework.authtoken.views import obtain_auth_token
+
 from api.views import RegistrationAPIView
-#from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api-auth/',include('rest_framework.urls')),
-    #path('api/token_varify/',TokenVerifyView.as_view(),name='varify-token'),   
-    #path('auth/register/', RegistrationAPIView.as_view(), name='register'),
-    #path('auth/login/', TokenObtainPairView.as_view(), name='login'),
-    #path('auth/refresh-token', TokenRefreshView.as_view(), name='refreshtoken'),
-
+    
 ]
